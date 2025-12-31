f9277ab7c1b328b04b22959022b36058732dfd79
Abhiraj Mandal <abhirajmandal18@gmail.com>
2025-12-31 17:24:33 +0530

    Implement timeout for echo reading in ultrasonic sensor
    Added timeout safety to pulseIn function for echo reading.

--- arduino/ultrasonic_distance_measurer/src/main.cpp
+++ arduino/ultrasonic_distance_measurer/src/main.cpp
@@ -27,7 +27,7 @@ void loop() {
   digitalWrite(trigPin, LOW);
 
   // Read echo
-  long duration = pulseIn(echoPin, HIGH); 
+  long duration = pulseIn(echoPin, HIGH, 30000); // timeout safety
   float distance = duration * 0.034 / 2;
 
   Serial.print("Distance: ");
